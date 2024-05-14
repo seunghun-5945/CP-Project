736324248fa583e32bfccbd31c92763871fa55df
tmdgns59451 <tmdgns5945@oasis.inje.ac.kr>
2024-05-14 14:57:10 +0900

    feat: change Login page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,6 +6,7 @@ import SubBanner from "../components/SubBanner";
 import ItemFrame from "../components/ItemFrame";
 import axios from "axios";
 import DailyModal from "../components/DailyModal";
+import { ImTelegram } from "react-icons/im";
 
 const Container = styled.div`
   width: 1400px;
@@ -29,6 +30,13 @@ const HomeContent = () => {
   const [info, setInfo] = useState([]);
   const [data, setData] = useState(null);
   const [isModalOpen, setIsModalOpen] = useState(true);
+  const [image, setImages] = useState([]);
+  const [FILE_nAME, setFILE_NAME] = useState([]);
+  FILE_nAME.length = 15;
+  let count, i = 0;
+  const API_KEY = 'AIzaSyAxAqJYZSKbF7pm5XHril-dndv4HdVrbz4';
+  const DRIVE_FOLDER_ID = '1-1w_h8t3ICtJRC57iUuTG-Mwy5sUFXJQ';
+  const FILE_NAME = '202404031640073ee624efd45c646d738d0ded223b2e5c.jpg'; 
 
   useEffect(() => {
     const fetchData = async () => {
@@ -39,14 +47,41 @@ const HomeContent = () => {
         console.error('Error fetching data:', error);
       }
     };
-  
     fetchData(); 
-  }, []); 
+  }, []);
   
   useEffect(() => {
-    console.log(info); 
-  }, [info]); 
+    info.forEach((item) => {
+      if(info.length > FILE_nAME.length)
+      {
+        FILE_nAME.push(item.picture);
+        fetchImages(FILE_nAME[0])
+      }
+      console.log(FILE_nAME[4]);
+      console.log(count);
+      count++;
+    });
+  }, [info]);
+  
 
+  const fetchImages = async (FILE_NAME) => {
+      try {
+        const response = await axios.get(`https://www.googleapis.com/drive/v3/files?q=name='${FILE_NAME}'+and+'${DRIVE_FOLDER_ID}'+in+parents&fields=files(id,name,thumbnailLink)`, {
+          params: {
+            key: API_KEY,
+            pageSize: 1
+          }
+        });
+        image.push(response.data.files[0]);
+        console.log(image);
+      } catch (error) {
+        console.error('Error fetching images:', error);
+      }
+  };
+   
+  const PictureReturn = () => {
+    return image.thumbnailLink;
+  }
   return (
     <Container>
       <DailyModal/>
@@ -55,25 +90,27 @@ const HomeContent = () => {
       <h2 style={{marginTop:"3%"}}>마감 인박 경매</h2>
       <Frame>
         {info.slice(0, 7).map((item, oldest) => (
+          
           <ItemFrame
             key={oldest}
             Product={item.title}
             Price={item.startprice}
-            Image={item.picture}
+            Image={PictureReturn()}
           />
         ))}
       </Frame>
       <h2 style={{marginTop:"3%"}}>최근에 올라온 경매</h2>
       <Frame>
-        {info.map((item, resent) => (
+        {info.slice(0, 7).map((item, eldest) => (
           <ItemFrame
-            key={resent}
-            Product={item.title}
-            Price={item.price}
-            Image={item.image}
+            key={eldest}
+            Image={image.thumbnailLink}
           />
         ))}
       </Frame>
+      
+      
+
     </Container>
   )
 }
--- src/pages/Test.js
+++ src/pages/Test.js
@@ -1,105 +1,42 @@
-import React, { useState } from 'react';
-import styled from 'styled-components';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const Container = styled.div`
-  width: 100%;
-  height: 100vh;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  background-color: salmon;
-`;
+const API_KEY = 'AIzaSyAxAqJYZSKbF7pm5XHril-dndv4HdVrbz4';
+const DRIVE_FOLDER_ID = '1-1w_h8t3ICtJRC57iUuTG-Mwy5sUFXJQ';
+const FILE_NAME = '202404031640073ee624efd45c646d738d0ded223b2e5c.jpg'; // 가져오고자 하는 파일 이름
 
-const Frame = styled.div`
-  width: 30%;
-  height: 40%;
-  border: 1px solid black;
-`;
+const App = () => {
+  const [image, setImage] = useState(null);
 
-const Box = styled.div`
-  width: 100%;
-  height: auto;
-  border: 1px solid black;
-`;
+  useEffect(() => {
+    const fetchImage = async () => {
+      try {
+        const response = await axios.get(`https://www.googleapis.com/drive/v3/files?q=name='${FILE_NAME}'+and+'${DRIVE_FOLDER_ID}'+in+parents&fields=files(id,name,thumbnailLink)`, {
+          params: {
+            key: API_KEY,
+            pageSize: 1
+          }
+        });
 
-function Test() {
-  const [password, setPassword] = useState('');
-  const [newPassword, setNewPassword] = useState('');
-  const [rePassword, setRepassword] = useState('');
-  const token = localStorage.getItem('token');
+        const imageData = response.data.files[0]; // 첫 번째 이미지만 가져오기
 
-  const handlePwChange = (e) => {
-    console.log(e.target.value);
-    setPassword(e.target.value);
-  }
-  
-  const handleNewPwChange = (e) => {
-    setNewPassword(e.target.value);
-  }
-  
-  const handleRePwChange = (e) => {
-    setRepassword(e.target.value);
-  }
+        setImage(imageData);
+      } catch (error) {
+        console.error('Error fetching image:', error);
+      }
+    };
 
-  const pwChange = async () => {
-
-    try {
-      const response = await axios.post('https://port-0-cpbeck-hdoly2altu7slne.sel5.cloudtype.app' + '/api/users/changing_password', {
-        data: {
-          user_password: password,
-          password: newPassword,
-          confirm_password: rePassword,
-          authorization: token
-        }
-      })
-      console.log('작동함');
-    }
-    catch (error) {
-      console.log('에러남');
-    }
-  }
- 
-  
-  const onWithDrawal = async () => {
-    try {
-      const response = await axios.post('https://port-0-cpbeck-hdoly2altu7slne.sel5.cloudtype.app/api/users/delete_user', {
-        data: {
-          authorization: token
-        }
-      });
-      console.log('서버로부터 응답:', response.data);
-      return response.data;
-    } catch (error) {
-      console.error('요청 중 오류 발생:', error);
-      throw error;
-    }
-  }
-  
+    fetchImage();
+  }, []);
 
   return (
-    <Container>
-      <Frame>
-        <Box>
-          <span>비밀번호입력</span><input type="text" onChange={handlePwChange}/>
-        </Box>
-        <Box>
-          <span>변경할 비밀번호입력</span><input type="text" onChange={handleNewPwChange}/>
-        </Box>
-        <Box>
-          <span>변경할 비밀번호 재입력</span><input type="text" onChange={handleRePwChange}/>
-        </Box>
-        <button onClick={pwChange}>변경하기</button>
-      </Frame>
-
-      <Frame>
-        <Box>
-          <span>회원탈퇴 테스트임</span><button onClick={onWithDrawal}>탈퇴하기</button>
-        </Box>
-      </Frame>
-    </Container>
+    <div>
+      <h1>Image from Google Drive</h1>
+      {image && (
+        <img src={image.thumbnailLink} alt={image.name} style={{wiedth:"1920px", height:"800px"}}/>
+      )}
+    </div>
   );
-}
+};
 
-export default Test;
+export default App;
