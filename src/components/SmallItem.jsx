ee0e4882b65b83b3f078805d1e0abd95a34194c3
tmdgns59451 <tmdgns5945@oasis.inje.ac.kr>
2024-05-21 19:18:16 +0900

    feat: complete design hotdeal page

--- src/components/SmallItem.jsx
+++ src/components/SmallItem.jsx
@@ -11,36 +11,65 @@ margin-top: 20px;
 border: 1px solid black;
 `;
 
-const ImgBox = styled.div`
-width: 20%;
-height: 80%;
-border: 1px solid black;
-`;
+  const ImgBox = styled.div`
+  width: 20%;
+  height: 80%;
+  border: 1px solid black;
+  display: flex;
+  align-items: flex-end;
+  `;
 
-const TextBox = styled.div`
-width: 40%;
-height: 80%;
-border: 1px solid black;
-`;
+  const TextBox = styled.div`
+  width: 40%;
+  height: 80%;
+  `;
 
-const StyledButton = styled.div`
-width: 100%;
-height: 30%;
-border: 1px solid gray;
-background-color: salmon;
-display: flex;
-align-items: center;
-justify-content: center;
-font-size: 20px;
-color: white;
-`;
+    const TitleTextArea = styled.div`
+      width: 100%;
+      height: 40%;
+      border-bottom: 1px solid lightgray;
+    `;
+
+    const ExplanationArea = styled.div`
+      width: 100%;
+      height: 30%;
+    `;
+
+    const PriceArea = styled.div`
+      width: 100%;
+      height: 30%;
+    `;
+
+  const StyledButton = styled.div`
+  width: 100%;
+  height: 30%;
+  border-radius: 15px;
+  background-color: salmon;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 20px;
+  color: white;
+  `;
 
 const SmallItem = () => {
   return (
     <SmallFrame>
       <ImgBox/>
-      <TextBox/>
-      <ImgBox/>
+      <TextBox>
+        <TitleTextArea>
+          <h3>이런 상품은 어떠신가요?</h3>
+        </TitleTextArea>
+        <ExplanationArea>
+          <h4>해병3분 짜장</h4>
+        </ExplanationArea>
+        <PriceArea>
+          <span>현재 시작가: 100원</span>
+        </PriceArea>
+      </TextBox>
+      <ImgBox style={{border:"none"}}>
+        <StyledButton>입찰하기</StyledButton>
+      </ImgBox>
     </SmallFrame>
   );
 };
--- src/pages/Hotdeal.js
+++ src/pages/Hotdeal.js
@@ -7,8 +7,6 @@ const Container = styled.div`
   width: 1400px;
   height: 100%;
   display: flex;
-  justify-content: flex-start;
-  align-items: center;
   flex-direction: column;
 `;
 
@@ -33,28 +31,60 @@ const Container = styled.div`
     const TextBox = styled.div`
       width: 50%;
       height: 80%;
-      border: 1px solid black;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: flex-start;
     `;
+
+      const TitleTextArea = styled.div`
+        width: 100%;
+        height: 40%;
+        border-bottom: 1px solid lightgray;
+        display: flex;
+      `
+
+      const ExplanationArea = styled.div`
+        width: 100%;
+        height: 30%;
+        display: flex;
+      `;
+
+      const PriceArea = styled.div`
+        width: 100%;
+        height: 30%;
+      `;
 
     const StyledButton = styled.div`
       width: 100%;
       height: 30%;
-      border: 1px solid gray;
+      border-radius: 20px;
       background-color: salmon;
       display: flex;
       align-items: center;
       justify-content: center;
       font-size: 20px;
       color: white;
+      cursor: pointer;
     `;
 
   const LargeItem = () => {
     return (
       <LargeFrame>
         <ImgBox />
-        <TextBox />
+        <TextBox>
+          <TitleTextArea>
+          <h1 style={{whiteSpace: "pre-wrap"}}>TODAY </h1><h1>PICK</h1>
+          </TitleTextArea>
+          <ExplanationArea>
+            <h3>요즘 제일 핫해! 어떤어떤 아이템</h3>
+          </ExplanationArea>
+          <PriceArea>
+            <h2>현재 시작가: 100000원</h2>
+          </PriceArea>
+        </TextBox>
         <ImgBox style={{border: "none"}}>
-          <StyledButton>핫딜상품 구매하기</StyledButton>
+          <StyledButton>핫딜상품 입찰하기</StyledButton>
         </ImgBox>
     </LargeFrame>
     );
@@ -73,6 +103,11 @@ const HotdealContent = () => {
   return (
     <Container>
       <LargeItem/>
+      <ExplanationArea>
+      <h2 style={{marginTop:"15px", whiteSpace: "pre-wrap"}}>핫딜 추천 상품 </h2>
+      <h3 style={{marginTop:"20px"}}>| BEBID 에서 현재 인기있는 경매품목입니다.</h3>
+      </ExplanationArea>
+
       <SmallFrameArea>
         <SmallItem/>
         <SmallItem/>
