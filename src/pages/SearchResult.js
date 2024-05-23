a9349c6b630431eb34da2657d330da9d029c7385
seunghun_lee <tmdgns5945@oasis.inje.ac.kr>
2024-05-24 05:58:48 +0900

    feat: hot deal axios active complete

--- src/pages/SearchResult.js
+++ src/pages/SearchResult.js
@@ -7,16 +7,37 @@ const Container = styled.div`
   width: 100%;
   height: auto;
   background-color: white;
+`;
+
+const SearchBox = styled.div`
+  width: 100%;
+  height: 100vh;
+`;
+
+const SmallArea = styled.div`
+  width: 100%;
+  height: 100px;
+  border-bottom: 2px solid black;
+`;
+
+const LargeArea = styled.div`
+  width: 100%;
+  height: 300px;
   border: 1px solid black;
 `;
 
-const SearchResultContent = ( {propsText} ) => {
-
-  const [searchText, setSearchText] = useState("");
+const SearchResultContent = (  ) => {
 
   return (
     <Container>
-      <h1>{"'" + {propsText} + "'" + "검색결과"}</h1>
+      <SmallArea>
+        <h1>'가방' 검색결과</h1>
+      </SmallArea>
+      <LargeArea>
+        <SmallArea>카테고리</SmallArea>
+        <SmallArea>가격</SmallArea>
+        <SmallArea>옵션</SmallArea>
+      </LargeArea>
     </Container>
   );
 };
